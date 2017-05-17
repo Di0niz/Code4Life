e7032f4ecdcf82de28f809e94ed4f686a0a425b9
Denis Moskovchenko <moskovchenkody@gmail.com>
2017-05-18 00:09:41 +0300

    изменения выбора молекул

--- app.py
+++ app.py
@@ -2,6 +2,92 @@
 import sys
 import random
 import math
+import itertools
+
+
+class Molecule(object):
+    def __init__(self, a=0, b=0, c=0, d=0, e=0):
+        self.a, self.b, self.c, self.d, self.e = a, b, c, d, e
+    
+    def add(self, other):
+        return Molecule(self.a + other.a, self.b + other.b, self.c + other.c, self.d + other.d, self.e + other.e)
+
+    def sub(self, other):
+        return Molecule(self.a - other.a, self.b - other.b, self.c - other.c, self.d - other.d, self.e - other.e)
+
+    def submodule(self, other):
+        a = max(self.a - other.a, 0)
+        b = max(self.b - other.b, 0)
+        c = max(self.c - other.c, 0)
+        d = max(self.d - other.d, 0)
+        e = max(self.e - other.e, 0)
+        return Molecule(a, b, c, d, e)
+
+    def min(self):
+        return min([self.a,self.b,self.c,self.d,self.e])
+        
+    def max(self):
+        return max([self.a,self.b,self.c,self.d,self.e])
+        
+    def sum(self):
+        return sum([self.a,self.b,self.c,self.d,self.e])
+
+    def diffrent(self):
+        return sum([
+            1 if self.a > 0 else 0,
+            1 if self.b > 0 else 0,
+            1 if self.c > 0 else 0,
+            1 if self.d > 0 else 0,
+            1 if self.e > 0 else 0]
+            )
+
+    def complexity(self):
+        return self.sum()/self.diffrent()
+
+    def diagnosed(self):
+        return not (self.a == -1 or self.b == -1 or self.c == -1 or self.d == -1 or self.e == -1)
+    
+    def __repr__(self):
+        return "%d %d %d %d %d" % (self.a,self.b,self.c,self.d,self.e)
+    
+    def letter(self, base=0):
+        char = None
+        if self.a >= base:
+            char = 'A'
+        elif self.b >= base:
+            char = 'B'
+        elif self.c >= base:
+            char = 'C'
+        elif self.d >= base:
+            char = 'D'
+        elif self.e >= base:
+            char = 'E'
+        return char
+
+    def first_letter(self):
+        return self.letter()
+    
+    def min_letter(self):
+        return self.letter(self.min())
+
+    def max_letter(self):
+        return self.letter(self.max())
+
+
+    @staticmethod
+    def parse(char):
+        ret = None
+        if char == 'A':
+            ret = Molecule(1,0,0,0,0)
+        elif char == 'B':
+            ret = Molecule(0,1,0,0,0)
+        elif char == 'C':
+            ret = Molecule(0,0,1,0,0)
+        elif char == 'D':
+            ret = Molecule(0,0,0,1)
+        elif char == 'E':
+            ret = Molecule(0,0,0,0,1)
+        return ret
 
 
 class ModuleType(object):
@@ -20,45 +106,26 @@ class Module(object):
         self.target = target
         self.eta = int(eta)
         self.score = int(score)
-        self.storage = {}
-        self.storage['a'] = int(storage_a)
-        self.storage['b'] = int(storage_b)
-        self.storage['c'] = int(storage_c)
-        self.storage['d'] = int(storage_d)
-        self.storage['e'] = int(storage_e)
-        self.expertise = {}
-        self.expertise['a'] = int(expertise_a)
-        self.expertise['b'] = int(expertise_b)
-        self.expertise['c'] = int(expertise_c)
-        self.expertise['d'] = int(expertise_d)
-        self.expertise['e'] = int(expertise_e)
 
-        self.count_molecules = sum(self.storage.values())
+        self.storage = Molecule(int(storage_a), int(storage_b), int(storage_c), int(storage_d), int(storage_e))
+        self.expertise = Molecule(int(expertise_a), int(expertise_b), int(expertise_c), int(expertise_d), int(expertise_e))
 
-        self.expected_molecules = sum(self.expertise.values())
+        self.count_molecules = self.storage.sum()
+
+        self.expected_molecules = self.expertise.sum()
 
 
     def find_molecules(self, samples):
         """Определение количества требуемых"""
-        molecules = {'a':0,'b':0,'c':0,'d':0,'e':0}
+        molecules = Molecule()
         # sum molecules
         for sample in samples:
             if sample.diagnosed:
-                for key in sample.cost:
-                    molecules[key] = molecules[key] + sample.cost[key]
-
+                molecules = molecules.add(sample.cost)
         # div 
-        for key in self.storage:
-            molecules[key] = molecules[key] - self.storage[key]
-            # вычитаем очки опыта
-            molecules[key] = molecules[key] - self.expertise[key]
+        molecules =  molecules.sub(self.storage).sub(self.expertise)
 
-        res = {}
-        for key in molecules:
-            if molecules[key] > 0:
-                res[key] = molecules[key]
-
-        return res
+        return molecules.first_letter()
 
 
     def find_availables(self, samples):
@@ -66,22 +133,25 @@ class Module(object):
 
         availables = []
 
-        molecules = {'a':0,'b':0,'c':0,'d':0,'e':0}
-        for key in self.storage:
-            molecules[key] = molecules[key] + self.storage[key] + self.expertise[key]
-
-        # sum molecules
+        storage = self.storage
+        expertise = self.expertise
+    
+        # sum storage
         for sample in sorted(samples, key=lambda x: x.complexity):
             if sample.diagnosed:
                 av_sample = True
-                for key in sample.cost:
-                    if molecules[key] - sample.cost[key] < 0:
-                        av_sample = False
+
+                cost = sample.cost.submodule(expertise)
+
+                if storage.sub(cost).min() < 0:
+                    av_sample = False
 
                 if av_sample == True:
                     availables.append(sample)
-                    for key in sample.cost:
-                        molecules[key] = molecules[key] - sample.cost[key]
+                    storage = storage.sub(cost)
+                    expertise = expertise.add(sample.gain)
+
+                    # добавить возможность определение возможности
 
         return availables
 
@@ -90,25 +160,28 @@ class Module(object):
 
         availables = []
 
-        molecules = {'a':0,'b':0,'c':0,'d':0,'e':0}
-        for key in self.storage:
-            molecules[key] = molecules[key] + self.storage[key] + self.expertise[key] + world.available[key]
+        storage = self.storage.add(world.available)
+
+        expertise = self.expertise
+
+        molecules = self.storage.add(self.expertise).add(world.available)
 
         # sum molecules
         # Необходимо добавить сортировку по среднему отклонению
         for sample in sorted(samples, key=lambda x: x.complexity):
             if sample.diagnosed:
                 av_sample = True
-                for key in sample.cost:
-                    if molecules[key] - sample.cost[key] < 0:
-                        av_sample = False
+
+                cost = sample.cost.submodule(expertise)
+
+                check_sub = molecules.sub(cost)
+                if check_sub.min() < 0:
+                    av_sample = False
 
                 if av_sample:
-
                     availables.append(sample)
-
-                    for key in sample.cost:
-                        molecules[key] = molecules[key] - sample.cost[key]
+                    storage = molecules.sub(cost)
+                    expertise = expertise.add(sample.gain)
 
         return availables
 
@@ -126,32 +199,28 @@ class Sample(object):
         self.carried_by = int(carried_by)
         self.rank = int(rank)
         self.health = int(health)
-        self.cost = {}
-        self.cost['a'] = int(cost_a)
-        self.cost['b'] = int(cost_b)
-        self.cost['c'] = int(cost_c)
-        self.cost['d'] = int(cost_d)
-        self.cost['e'] = int(cost_e)
+        self.cost = Molecule(int(cost_a),int(cost_b),int(cost_c),int(cost_d),int(cost_e))
+        self.gain = Molecule.parse(expertise_gain)
 
-        self.diagnosed = (int(cost_a) != -1)
+        self.diagnosed = self.cost.diagnosed()
 
         if self.diagnosed:
-
-            self.complexity = sum(self.cost.values()) / len([x for x in self.cost.values() if x > 0])
+            self.complexity = self.cost.complexity()
         else: 
             self.complexity = 999
 
     def __repr__(self):
-        return "%d-%d: %d %d %d %d %d" % (self.sample_id,self.rank, self.cost['a'], self.cost['b'],self.cost['c'],self.cost['d'], self.cost['e'])
+        return "%2d-%d: %s" % (self.sample_id,self.rank, self.cost)
 
 class World(object):
     def __init__(self):
         project_count = int(raw_input())
+        self.projects = []
         for i in xrange(project_count):
             raw = raw_input()
             print >> sys.stderr, raw
 
-            a, b, c, d, e = [int(j) for j in raw.split()]
+            self.projects.append([int(j) for j in raw.split()])
 
     def update(self):
         self.modules = []
@@ -160,15 +229,10 @@ class World(object):
             module.update()
             self.modules.append(module)
         raw = raw_input()
-        
+
         print >> sys.stderr, raw
-        available_a, available_b, available_c, available_d, available_e = [int(i) for i in raw.split()]
-        self.available ={}
-        self.available['a'] = available_a
-        self.available['b'] = available_b
-        self.available['c'] = available_c
-        self.available['d'] = available_d
-        self.available['e'] = available_e
+        a,b,c,d,e = [int(i) for i in raw.split()]
+        self.available = Molecule(a,b,c,d,e)
 
 
         self.own_samples = []
@@ -187,7 +251,7 @@ class World(object):
 
     def check_available(self, molecules):
         available = True
-        for key in molecules:
+        for key in xrange(5):
             if self.available[key] < molecules[key]:
                 available = False
 
@@ -281,6 +345,42 @@ class Strategy(object):
 
         return molecule
 
+    def next_sample(self):
+        results = []
+        for available in self.availables:
+
+            # перебираем доступные комбинации
+            for combination in itertools.permutations([x for x in self.diagnosed if x != available]):
+
+                storage = self.target.storage
+                expertise = self.target.expertise
+
+                cost = available.cost.submodule(expertise)
+                storage = storage.sub(cost)
+                expertise = expertise.add(available.gain)
+
+                step = 0
+                results.append((step, storage.sum(), available))
+
+                for sample in combination:
+
+                    cost = sample.cost.submodule(expertise)
+                    storage = storage.sub(cost)
+                    expertise = expertise.add(sample.gain)
+
+
+                    if storage.min() < 0:
+                        break
+
+                    step = step + 1
+                    results.append((step, storage.sum(), available))
+
+
+        sample = sorted(results, key=lambda x: (x[0],x[1])).pop()
+
+        return sample[2]
+        
+
     def get_action(self):
 
         command = None
@@ -348,8 +448,8 @@ class Strategy(object):
             elif action == Actions.MOLECULES_CONNECT:
                 molecules = self.target.find_molecules(self.potentials[:1])
 
-                if self.target.count_molecules < 10 and len(molecules.keys()) > 0:
-                    command = (Commands.CONNECT, molecules.popitem()[0], action)
+                if self.target.count_molecules < 10 and molecules is not None:
+                    command = (Commands.CONNECT, molecules, action)
                 #elif self.target.molecules < 10:
                 #    action = Actions.MOLECULES_GREED
                 else:
@@ -365,7 +465,7 @@ class Strategy(object):
 
             elif action == Actions.MOLECULES_TO_LABORATORY:
                 command = (Commands.LABORATORY, None, action)
-            
+
             #
             # LABORATORY
             #
@@ -386,7 +486,7 @@ class Strategy(object):
             elif action == Actions.LABORATORY_TO_SAMPLES:
                 command = (Commands.SAMPLES, None, action)
             elif action == Actions.LABORATORY_CONNECT:
-                command = (Commands.CONNECT, self.availables.pop().sample_id, action)
+                command = (Commands.CONNECT, self.next_sample().sample_id, action)
 
             else:
                 # Дорабатываем систему
--- testing.py
+++ testing.py
@@ -3,38 +3,136 @@ import unittest
 import sys
 import StringIO
 from app import Sample, Module, World, Strategy
+import itertools
 
 
 class TestStringMethods(unittest.TestCase):
-    def test_potential(self):
+    def test1_potential(self):
         """Проверяем вычисление потенциальных точек"""
 
         sys.stdin = StringIO.StringIO('0')
 
         w = World()
 
-        sys.stdin = StringIO.StringIO("""DIAGNOSIS 0 0 0 0 0 0 0 0 0 0 0 0
-        MOLECULES 0 0 0 1 0 0 0 0 0 0 0 0
-        6 5 6 6 6
-        4
-        1 0 2 D 30 0 0 0 6 0
-        2 0 2 E 20 0 0 5 3 0
-        3 0 2 D 10 0 3 0 2 3
-        0 1 2 E 20 0 1 4 2 0
+        sys.stdin = StringIO.StringIO("""LABORATORY 2 12 2 3 2 1 2 1 0 1 0 1
+MOLECULES 2 4 0 0 0 2 0 0 0 1 2 1
+3 2 3 2 3
+6
+8 0 1 A 1 0 1 2 1 1
+9 0 1 A 1 0 2 0 0 2
+10 0 1 D 10 4 0 0 0 0
+6 1 1 E 1 2 2 0 1 0
+11 1 1 B 1 1 0 0 0 2
+12 1 1 E 1 1 1 1 1 0
+
         """)
 
         w.update()
 
         s = Strategy(w)
 
+
+        command = s.get_action()
+
+    pass
+
+    def test1_find_order(self):
+
+        sys.stdin = StringIO.StringIO('0')
+
+        w = World()
+
+        # sys.stdin = StringIO.StringIO("""LABORATORY 2 12 2 3 2 1 2 1 0 1 0 1
+        # MOLECULES 2 4 0 0 0 2 0 0 0 1 2 1
+        # 3 2 3 2 3
+        # 4
+        # 8 0 1 A 1 0 1 2 1 1
+        # 9 0 1 A 1 0 2 0 0 2
+        # 10 0 1 D 10 4 0 0 0 0
+        # 6 1 1 E 1 2 2 0 1 0
+        # 11 1 1 B 1 1 0 0 0 2
+        # 12 1 1 E 1 1 1 1 1 0
+        # """)
+        sys.stdin = StringIO.StringIO("""LABORATORY 0 0 3 1 2 3 0 0 0 0 0 0
+        LABORATORY 0 0 2 2 3 2 0 0 0 0 0 0
+        0 2 0 0 5
+        6
+        0 0 1 C 1 0 0 0 3 0
+        2 0 1 A 10 0 0 4 0 0
+        4 0 1 E 1 2 0 2 0 0
+        1 1 1 E 1 0 0 3 0 0
+        3 1 1 C 1 2 1 0 0 0
+        5 1 1 D 1 0 2 1 0 0""")
+
+        w.update()
+        
+        s = Strategy(w)
+
+        strong_solution = None
+        weak_solution = None
+
+        results = []
+        for available in s.availables:
+
+            # перебираем доступные комбинации
+            for combination in itertools.permutations([x for x in s.diagnosed if x != available]):
+
+                storage = s.target.storage
+                expertise = s.target.expertise
+
+                cost = available.cost.submodule(expertise)
+                storage = storage.sub(cost)
+                expertise = expertise.add(available.gain)
+
+                step = 0
+                results.append((step, storage.sum(), available))
+
+                for sample in combination:
+
+                    cost = sample.cost.submodule(expertise)
+                    storage = storage.sub(cost)
+                    expertise = expertise.add(sample.gain)
+
+
+                    if storage.min() < 0:
+                        break
+
+                    step = step + 1
+                    results.append((step, storage.sum(), available))
+
+
+        sample = sorted(results, key=lambda x: (x[0],x[1])).pop()
+        print sample
+
         print s.availables
         print s.potentials
         print s.diagnosed
 
+
         command = s.get_action()
         print command
 
-    pass
+
+    def test_choose_molecule(self):
+        sys.stdin = StringIO.StringIO('0')
+
+        w = World()
+
+
+        sys.stdin = StringIO.StringIO("""MOLECULES 1 24 0 0 1 1 1 3 0 1 1 1
+        DIAGNOSIS 0 6 0 1 0 2 0 0 1 1 2 2
+        5 4 4 2 4
+        6
+        13 0 2 B 20 2 0 0 1 4
+        14 0 2 A 20 0 0 1 4 2
+        15 0 2 A 20 0 0 0 5 0
+        6 1 1 E 1 2 2 0 1 0
+        16 1 2 0 -1 -1 -1 -1 -1 -1
+        17 1 2 0 -1 -1 -1 -1 -1 -1
+        """)
+        w.update()
+        
+        s = Strategy(w)
 
 if __name__ == '__main__':
     unittest.main()
